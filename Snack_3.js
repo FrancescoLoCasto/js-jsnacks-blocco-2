2014795df8bb8657c5522f3737d0fe1088337e05
MyUsername <my@email.com>
2022-11-25 16:27:21 +0100

    complete Snack_3

---
+++ Snack_3.js
@@ -0,0 +1,12 @@
+//Calcola la somma dei primi 10 numeri di un array.
+
+   const arrayNumber = [12,23,67,89,13,98,134,29,987,234]
+
+   let allNumbers = 0
+
+
+   for (let i = 0; i < 10; i++) {
+      allNumbers += arrayNumber[i];
+   }
+   
+console.log(allNumbers);
