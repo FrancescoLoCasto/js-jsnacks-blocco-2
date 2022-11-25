8a83ccdfc6b60b0a62d32da445987a56b6b23892
MyUsername <my@email.com>
2022-11-25 15:40:42 +0100

    Snack_1 complete

---
+++ Snack_1.js
@@ -0,0 +1,16 @@
+   //Crea un array vuoto.
+   //Chiedi per 6 volte all'utente di inserire un numero,
+   //se è dispari inseriscilo nell'array.
+
+   const arrayNumber = [];
+
+
+   for(let i = 0; i < 6; i++) {
+      const numberUser = prompt("Inserisci il tuo numero")
+
+      if (numberUser % 3 == 1){
+         arrayNumber.push(numberUser)
+     }
+   }
+
+   console.warn(arrayNumber)
