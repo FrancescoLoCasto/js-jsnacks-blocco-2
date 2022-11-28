053fe58b9855d03b312ef744c47ac214890be992
MyUsername <my@email.com>
2022-11-28 11:02:17 +0100

    complete snack_5

---
+++ Snack_5_Morning.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * Generatore di persone fittizie casuali:
+ * Il Grande Gatsby ha una lista di nomi e una lista di cognomi, da queste vuole generare una falsa lista di invitati con nome e cognome.
+ *
+ */
+
+ const names = ['Michela', 'Giuseppina', 'Taylor','Donna', 'Marsha', 'Fabio', 'Roberto'];
+ const lastnames = ['Cappelletti', 'Jager', 'Porcinelli' , 'Buffetti' , 'Forghieri', 'Papagni', 'Marazzini'];
+
+ let falseNames = [];
+
+ for (let i = 0 ; i < 10; i++){
+ 
+   falseNames = names + lastnames [Math.floor(Math.random() * 10)];
+ 
+   console.log(falseNames);
+ }
